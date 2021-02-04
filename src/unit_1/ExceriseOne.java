726d5a4ca25b538e2d3a75656d5b943d3b6386de
Kurt <52134197+KurtHilder@users.noreply.github.com>
2021-02-04 10:56:39 +0200

    04-02 ex 1

---
+++ src/unit_1/ExceriseOne.java
@@ -0,0 +1,29 @@
+package unit_1;
+
+public class ExceriseOne {
+	public static void main(String[] args) {
+		
+		char letter = '\t';
+		int num = 32;
+		System.out.println((char)9 + Character.isWhitespace(letter));
+		System.out.println(Character.isWhitespace(num));
+		
+//		int check = 102;
+		System.out.println((int)letter);
+//		
+//		char space = ' ';
+//		System.out.println((int)space);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
