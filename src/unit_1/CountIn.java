78b8ccc241f52473bfce7d8fd328088fccba3f6c
Kurt <52134197+KurtHilder@users.noreply.github.com>
2021-03-01 15:33:01 +0200

    CountIn update

--- src/unit_1/CountIn.java
+++ src/unit_1/CountIn.java
@@ -1,18 +1,30 @@
 package unit_1;
 
+//import javax.swing.*;
+
 public class CountIn {
 	public static void main(String[] args) {
+
+		String sentence = "In the ping i'm going to ink a linkin";
+		String lookfor = "in";
 		
-		String sentence = "In the ping i'm going to ink a link";
 		
 		int len = sentence.length();
+		sentence = sentence + " "; // add extra space to for loop to work
+		
 		int count = 0;
-		for(int i = 0; i < len;i++) {
-			
-			if(sentence.subSequence(i, i+2).equals("in")) {
-				count++;
+		
+		for(int i = 0; i < len ; i++) {
+			String twoLetters = sentence.substring(i,i+2); // splits the string into two characters each time.
+			boolean hasIn = twoLetters.equalsIgnoreCase(lookfor); // checks if it contains the predefines letter combination
+			if (hasIn) {
+				count++; // increments the value if there 
+				System.out.println(twoLetters + " 1 " + count);
+			} else {
+				System.out.println(twoLetters + " 0 ");
 			}
 		}
-		System.out.println(count);
+		System.out.println("The total number of \"" + lookfor +"\" in the string is: " + count);
+		
 	}
 }
---
+++ src/unit_1/EmailCheck.java
@@ -0,0 +1,30 @@
+package unit_1;
+
+public class EmailCheck {
+
+	public static void main(String[] args) {
+		
+		String email = "khilder@gmail.com";
+		String phone = "0829203298";
+		String name = "Kurt Hilder";
+		// find the user name of the email address
+		// find the email provider
+		// check if phone number has 10 digits.
+		// split name into first and last(uppercase).
+		
+		// CODE GOES HERE
+		
+		
+		
+		// print out
+		System.out.println("UserName: " /* + var */);
+		System.out.println("EmailSer: " /* + var */);
+		System.out.println("ValidNum: " /* + var */);
+		System.out.println("FirstNam: " /* + var */);
+		System.out.println("LastName: " /* + var */);
+		
+		
+
+	}
+
+}
