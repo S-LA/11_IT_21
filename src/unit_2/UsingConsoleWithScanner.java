d1f6f339f26670c4c5111196cc5e818844c84eab
Kurt <52134197+KurtHilder@users.noreply.github.com>
2021-04-13 13:23:17 +0200

    mike

---
+++ src/unit_2/UsingConsoleWithScanner.java
@@ -0,0 +1,10 @@
+package unit_2;
+import java.util.*;
+
+public class UsingConsoleWithScanner {
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		String input = console.nextLine();
+		System.out.print(input);
+	}
+}
