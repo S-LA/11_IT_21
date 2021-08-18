7453ba0db39bb03cb9b881a560d30de770242d97
Kurt <52134197+KurtHilder@users.noreply.github.com>
2021-08-18 13:35:49 +0200

    Update Arithmetic.java

--- OtherInfo/Arithmetic.java
+++ OtherInfo/Arithmetic.java
@@ -3,66 +3,55 @@
    
       private double num1, num2;
    
-      public Arithmetic () // Ex 2.2.1
-      {
+      public Arithmetic ()      {
          num1 = 0;
          num2 = 0;
       }
    
    
-      public Arithmetic (double n1, double n2) // Ex 2.2.2
-      {
+      public Arithmetic (double n1, double n2)       {
          num1 = n1;
          num2 = n2;
       }
       
-      public void display() // Ex 2.2.3 - (Must be removed for Exercise 5 No 2)
-      {
+      public void display()       {
          System.out.println("Num 1: " + num1 + " Num 2: " + num2);
       }
    
    
-      public void setNum1 (double n1) // Ex 2.3.1
-      {
+      public void setNum1 (double n1)      {
          num1 = n1;
       }
    
    
-      public void setNum2 (double n2) // Ex 2.3.1
-      {
+      public void setNum2 (double n2)      {
          num2 = n2;
       }
    
    
-      public double getNum1 () //Exercise 3
-      {
+      public double getNum1 ()       {
          return num1;
       }
    
    
-      public double getNum2 () //Exercise 3
-      {
+      public double getNum2 ()       {
          return num2;
       }
    
    
-      public double add () //Activity 11
-      {
+      public double add ()       {
          return num1 + num2;
       }
     
-      public double subtract () //Exercise 4 No 1
-      {
+      public double subtract ()      {
          return num1 - num2;
       }
    
-      public double multiply () //Exercise 4 No 2
-      {
+      public double multiply ()       {
          return num1 * num2;
       }
    
-      public double divide () throws ArithmeticException //Exercise 4 No 2
-      {
+      public double divide () throws ArithmeticException   {
           if (num2 == 0){
               //System.out.println("Error: Can not divide by zero"); // Ex4 No 2.1
               throw new ArithmeticException();
@@ -74,8 +63,7 @@
    	
       
    
-      public String toString() //Exercise 5
-      {
+      public String toString()       {
          return "Num 1: " + num1 + "\nNum 2: " + num2;
       }
    	    
