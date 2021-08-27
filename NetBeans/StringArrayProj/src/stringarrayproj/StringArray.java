b02eb4f1b4ceab93ab12ff19255d68dcd12a448b
Kurt <52134197+KurtHilder@users.noreply.github.com>
2021-08-27 13:03:14 +0200

    wayne&dylan

---
+++ NetBeans/StringArrayProj/src/stringarrayproj/StringArray.java
@@ -0,0 +1,72 @@
+/*
+ *  SHANGRI-LA ACADEMY
+ *  INFORMATION TECHNOLOGY
+ *  CLASS NOTES
+ */
+package stringarrayproj;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kwhil
+ */
+public class StringArray {
+	private String[] strArr = new String[100];
+	private int size = 0;
+	
+	public StringArray(){
+		try{
+			//File f = new File("Names.txt");
+			String path = "src/stringarrayproj/Names.txt";
+			Scanner scFile = new Scanner(new File (path));
+			while(scFile.hasNext()){
+				strArr[size] = scFile.nextLine();
+				size++;
+			}
+			scFile.close();
+			
+		} catch( FileNotFoundException ex){
+			System.out.println("File not found");
+		}
+	}
+	
+	public String toString(){
+		
+		String temp = "";
+		for(int i = 0; i < size; i++ ){
+			temp = temp + strArr[i] + "\n";
+		}
+		return temp;
+	}
+	
+	public void selectionSort(){
+		for(int i = 0; i< size -1 ; i++){
+			for(int j = i+1 ; j < size; j++){
+				if(strArr[i].compareTo(strArr[j])>0){
+					String temp = strArr[i];
+					strArr[i] = strArr[j];
+					strArr[j] = temp;
+				}
+			}
+		}
+	}
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ NetBeans/StringArrayProj/src/stringarrayproj/StringArrayProj.java
@@ -0,0 +1,35 @@
+/*
+ *  SHANGRI-LA ACADEMY
+ *  INFORMATION TECHNOLOGY
+ *  CLASS NOTES
+ */
+package stringarrayproj;
+
+/**
+ *
+ * @author kwhil
+ */
+public class StringArrayProj {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		StringArray st = new StringArray();
+		System.out.println(st.toString());
+		
+		System.out.println("SORTING");
+		st.selectionSort();
+		System.out.println(st.toString());
+		
+		
+	}
+	
+}
+
+
+
+
+
+
