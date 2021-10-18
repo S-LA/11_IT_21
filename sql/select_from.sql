87b3cca29a300f049f53b8abbdb3c1212abc7989
KurtHilder <52134197+KurtHilder@users.noreply.github.com>
2021-10-18 14:32:03 +0200

    2021-10-18

---
+++ sql/select_from.sql
@@ -0,0 +1,3 @@
+/* GET THE NAME OF A STUDENT WHO GOT A PARTICULAR MARK*/
+
+select surname from it_marks where mark1='75';
---
+++ sql/update_name.sql
@@ -0,0 +1,5 @@
+/*  HOW TO UPDATE A SINGLE VALUE */
+
+update it_marks set surname='Bruce' where id=3;
+/*update it_marks set mark2='90' where id=1;*/
+/*update it_marks set mark2='90' where surname='John';*/
